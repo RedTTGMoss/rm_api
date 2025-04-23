e1f6fff759f23dc320cdc3e1e957ed0add8c3bdd
RedTTG <red@redttg.com>
2025-04-23 19:46:49 +0300

    Fix tectonic issue with rmfakecloud

--- rm_api/storage/new_sync.py
+++ rm_api/storage/new_sync.py
@@ -24,5 +24,7 @@ def get_documents_new_sync(api: 'API', progress):
 
 def handle_new_api_steps(api: 'API'):
     token_info = jwt.decode(api.token, algorithms=["HS256"], options={"verify_signature": False})
-    tectonic = token_info['tectonic']
+    tectonic = token_info.get('tectonic')
+    if not tectonic:
+        return  # We can skip the new API steps if tectonic is missing, it's most likely a custom API.
     api.document_notifications_uri = api.document_storage_uri = f'https://{tectonic}.tectonic.remarkable.com/'
